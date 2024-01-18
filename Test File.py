34964447951a65c9fd70d1ebc5aa49e030a96cc5
RunningRabbit12 <157074442+RunningRabbit12@users.noreply.github.com>
2024-01-18 16:21:23 -0700

    Add files via upload

---
+++ Test File.py
@@ -0,0 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.integrate import solve_bvp
+from scipy.stats import beta
+import time
+from math import sqrt
+
+
+
+
+
+print('hello world!')
